69dea25eba35e7b5f404f6bc6686484c91678dbd
Yuval Fidler <yuval.fid@gmail.com>
2023-01-11 22:40:00 +0200

    main and vars

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -40,11 +40,11 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.18.1"
 
-  name                 = "k8s-vpc"
-  cidr                 = "172.16.0.0/16"
+  name                 = var.name
+  cidr                 = var.vpc_cidr
   azs                  = data.aws_availability_zones.available.names
-  private_subnets      = ["172.16.1.0/24", "172.16.2.0/24", "172.16.3.0/24"]
-  public_subnets       = ["172.16.4.0/24", "172.16.5.0/24", "172.16.6.0/24"]
+  private_subnets      = var.vpc_private_subnets
+  public_subnets       = var.vpc_public_subnets
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
@@ -73,10 +73,10 @@ module "eks" {
   eks_managed_node_groups = {
     first = {
       desired_capacity = 1
-      max_capacity     = 10
+      max_capacity     = 1
       min_capacity     = 1
 
-      instance_type = "m5.large"
+      instance_type = "t3.medium"
     }
   }
 }
---
+++ infra/terraform/vpc_vars.tf
@@ -0,0 +1,19 @@
+variable "name" {
+  description = "vpc name"
+  type        = string
+  default     = "yf-tf-eks"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "vpc_private_subnets" {
+  type    = list(any)
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "vpc_public_subnets" {
+  type    = list(any)
+  default = ["10.0.101.0/24", "10.0.102.0/24"]
+}

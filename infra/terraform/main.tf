7fab4724f330a3f80841ebf5b873e820e56b4590
Yuval Fidler <yuval.fid@gmail.com>
2023-01-15 17:54:52 +0200

    main and vars

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -42,9 +42,11 @@ module "vpc" {
 
   name                 = var.name
   cidr                 = var.vpc_cidr
+
   azs                  = data.aws_availability_zones.available.names
   private_subnets      = var.vpc_private_subnets
   public_subnets       = var.vpc_public_subnets
+
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
@@ -86,7 +88,7 @@ module "eks" {
       min_capacity     = 1
 
       instance_type = "t3.medium"
-      capacity_type  = "ON_DEMAND"
+      capacity_type = "ON_DEMAND"
     }
   }
 }

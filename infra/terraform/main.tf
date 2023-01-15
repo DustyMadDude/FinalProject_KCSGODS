06aba8490cf3ce0005d49688f73620a633f85396
Yuval Fidler <yuval.fid@gmail.com>
2023-01-15 17:18:44 +0200

    main and vars

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -68,7 +68,6 @@ module "eks" {
   cluster_name    = local.cluster_name
   cluster_version = "1.24"
   subnet_ids      = module.vpc.private_subnets
-
   vpc_id = module.vpc.vpc_id
 
   eks_managed_node_groups = {
